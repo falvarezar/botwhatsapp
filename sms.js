9725340c1c35dbb7ce694f560802b39bbfb1baa4
Fabian Alvarez <falvarez_arnaldo@hotmail.com>
2022-05-15 11:54:20 -0400

    mensajes a whatsapp

--- sms.js
+++ sms.js
@@ -1,28 +1,41 @@
+const axios = require('axios');
+const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require("whatsapp-web.js");
-client = new Client({
-    authStrategy: new LocalAuth({
-        clientId: "clienteuno"
-    })
-});
-client.on("qr", (qr) => {
-    // Generate and scan this code with your phone
-    console.log("QR RECEIVED", qr);
-});
-client.on('ready', () => {
-    console.log('Client is ready!');
-    const number = "+595992441761";
 
-    const text = "Hola este es un sms de prueba";
-    const chatId = number.substring(1) + "@c.us";
+let number = "+595992441761";
+let chatId = number.substring(1) + "@c.us";
 
-    client.sendMessage(chatId, text);
-    
-});
+const ActivateSession = () => {
+    client = new Client({
+        authStrategy: new LocalAuth({
+            clientId: "clientesms"
+        })
+    });
 
-client.initialize();
+    client.on('qr', qr => {
+        qrcode.generate(qr, { small: true });
+    });
 
-/*client.isRegisteredUser("911234567890@c.us").then(function (isRegistered) {
-    if (isRegistered) {
-        client.sendMessage("911234567890@c.us", "hello");
-    }
-})*/
+    client.on('ready', (err) => {
+        console.log('Client is ready!');
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    client.initialize();
+}
+
+
+function intervalFunc() {
+    axios.post(`http://localhost/sms/consulta`)
+        .then(response => {
+            var toJSON = response.data
+            let text = toJSON.mensaje
+            client.sendMessage(chatId, text);
+        })
+}
+
+setInterval(intervalFunc, 20000);
+
+ActivateSession()
